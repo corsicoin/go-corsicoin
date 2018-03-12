6951c5f4af9ad94d5da5e61781c033b0486b57a4
corsicoin <sebcorse@gmail.com>
2018-03-12 11:42:13 +0100

    Modif defaults params & nodes

--- node/defaults.go
+++ node/defaults.go
@@ -42,8 +42,8 @@ var DefaultConfig = Config{
 	WSPort:           DefaultWSPort,
 	WSModules:        []string{"net", "web3"},
 	P2P: p2p.Config{
-		ListenAddr: ":30303",
-		MaxPeers:   25,
+		ListenAddr: ":30309",
+		MaxPeers:   10,
 		NAT:        nat.Any(),
 	},
 }
@@ -55,11 +55,11 @@ func DefaultDataDir() string {
 	home := homeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Corsicoin")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "Corsicoin")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".corsicoin")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
